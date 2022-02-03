57110c05a266118077d407c3f2f7126bb97a2d67
William Warner Burke <williamburke121@gmail.com>
2022-02-03 17:19:08 -0500

    commit tree b24f6f4558a28e1282d6fc2a1e71aa121f05768e

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((titleCase) =>{
+    return titleCase.split(" ").map((word) =>{
+      return word.charAt(0).toUpperCase() + word.slice(1)
+}).join(" ")
+  });
 }
